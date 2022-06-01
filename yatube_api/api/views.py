ca385c0c0d93231fe0036118b63665871bd0e798
Ilya Valeev <iljavaleev@gmail.com>
2022-06-01 21:15:25 +0300

    preclean

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import mixins, filters, viewsets
 
-from posts.models import Group, Post, Follow
+from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer,
